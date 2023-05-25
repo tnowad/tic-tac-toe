26b31ba4a2499f2d8daa166a9c96c6c0d5004485
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-26 00:51:00 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-
-  return (<div className='text-3xl font-bold text-blue-600'>
-    Hii
-  </div>
-  )
+  return <div className="text-3xl font-bold text-blue-600">Test</div>;
 }
 
-export default App
+export default App;
